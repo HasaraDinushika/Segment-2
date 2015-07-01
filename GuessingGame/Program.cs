142c1a57d8ff2cfd1f8cec72f1e4f0170e45ef18
HasaraDinushika <hgunasiri@gmail.com>
2015-07-01 10:35:12 +0530

    Complete assignment

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GuessNum();
+        }
+
+        static void GuessNum()
+        {
+            int numToGuess = 42;
+            int userGuess = 0;
+
+            while (userGuess != numToGuess)
+            {
+                Console.WriteLine("Enter your guess between 1 to 100");
+
+                bool validInput = int.TryParse(Console.ReadLine(), out userGuess);
+                bool validRange = (0 <= userGuess) && (userGuess <= 100);
+
+                if (!validInput || !validRange || (userGuess != numToGuess))
+                {
+                    Console.WriteLine("Incorrect");
+                }
+                else
+                {
+                    Console.WriteLine("Correct");
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
